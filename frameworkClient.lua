2a73d3b4c0c95f46b8f5db847f70c5550f991f74
CrosRoad95 <sebajura1234@gmail.com>
2020-11-26 16:11:53 +0100

    make publish create working mta resource

---
+++ frameworkClient.lua
@@ -0,0 +1,14 @@
+﻿local screenWidth, screenHeight = 600,600
+
+local window = guiCreateWindow( 200, 50, screenWidth, screenHeight, "Mta Blazor", false )
+local browser = guiCreateBrowser( 0, 28, screenWidth, screenHeight, true, false, false, window )
+local theBrowser = guiGetBrowser( browser )
+
+addEventHandler( "onClientBrowserCreated", theBrowser, 
+	function( )
+		loadBrowserURL( source, "http://mta/local/index.html" )
+		toggleBrowserDevTools(source, true)
+	end
+)
+
+setDevelopmentMode(true, true)
---
+++ postPublish.ps1
@@ -0,0 +1,37 @@
+﻿$resourcePath = $args[0]
+$metaXmlPath = $($args[0] + "..\meta.xml")
+$filesPath = $resourcePath + "..\wwwroot\"
+$files = Get-ChildItem -Recurse -af $filesPath
+Set-Location $filesPath
+
+$metaXmlContent = ""
+
+$nl = [Environment]::NewLine
+
+$metaXmlContent = $metaXmlContent + "<meta>" + $nl
+$metaXmlContent = $metaXmlContent + "`t<script src='_framework\frameworkClient.lua' type='client'/>" + $nl
+$metaXmlContent = $metaXmlContent + "`t<script src='_framework\frameworkServer.lua' type='server'/>" + $nl
+for ($i=0; $i -lt $files.Count; $i++) {
+	$fileName = $files[$i].fullName
+	if (!$fileName.EndsWith('.gz') -and !$fileName.EndsWith('.br'))
+	{
+		if(!$fileName.EndsWith('.lua'))
+		{
+			$fileName = (Get-Item $fileName | Resolve-Path -Relative).substring(2)
+			$file = "`t<file src='$fileName' />"
+			$metaXmlContent = $metaXmlContent + $relativePath + $file + $nl
+		}
+	}
+	else
+	{
+		Remove-Item $fileName
+	}
+}
+$metaXmlContent = $metaXmlContent + "</meta>"
+
+echo $metaXmlContent | Out-File -encoding ASCII $metaXmlPath
+
+Remove-Item $resourcePath -Recurse
+Get-ChildItem -Path ($resourcePath + "..\wwwroot\") -Recurse |  Move-Item -Destination ($resourcePath + "..\")
+
+Remove-Item ($resourcePath + "..\wwwroot")
